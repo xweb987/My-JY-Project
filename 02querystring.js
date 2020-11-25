1cd27284bf1c1db2ad6d60067139822220cab777
xweb987 <74405590+xweb987@users.noreply.github.com>
2020-11-25 19:51:56 +0800

    Delete 02querystring.js

--- 02querystring.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-app.get("/getUserInfo",(req,res)=>{
-    console.log(req.query);
-    res.send(req.query);
-})
-app.listen(8080,()=>{
-    console.log("server is running at http://127.0.0.1:8080");
-})
